46e4beec253db31f7905d2148e11451762e7af65
leiyonghui <729186631@qq.com>
2022-05-24 17:06:04 +0800

    msg

--- engine/Packet.h
+++ engine/Packet.h
@@ -6,13 +6,13 @@ namespace engine
 	class Packet
 	{
 	public:
-		Packet(EPacketType type, uint32 uid):_type(type),_uid(uid){}
+		Packet(int32 type, uint32 uid):_type(type),_uid(uid){}
 		virtual ~Packet() = default;
 
 		uint32 getUid() const { return _uid; };
 
 	protected:
-		EPacketType _type;
+		int32 _type;
 		uint32 _uid;
 	};
 }
